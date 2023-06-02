9bd8cd59a226f51307d8d588f9e5bd773cd884b8
Chesney Hardin <chesney.hardin@gmail.com>
2023-06-02 10:42:37 -0500

    working on employee list

---
+++ src/components/employees/EmployeeList.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react"
+
+export const EmployeeList = () => {
+    const [employees, setEmployees] = useState([])
+
+    useEffect(
+        ()=> {
+            return fetch('http://localhost:8088/users?isStaff=true')
+            .then(response => response.json)
+            .then((employeeArray) => {
+                setEmployees(employeeArray)
+            })
+        },
+        []
+    )
+
+    return <article className="employees">
+            <h2>List of Employess</h2>
+
+        {
+            employees.map(employee => {
+                return <section className="employee" key={`employee--${employee.id}`}>
+                    <div>Name: {employee.fullName}</div>
+                    <div>Email: {employee.email}</div>
+
+                </section>
+            })
+        }
+    </article>
+}
