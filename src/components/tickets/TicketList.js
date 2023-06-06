9a89b5da2f9a11895fd12b94cc9cade0618c51cf
Chesney Hardin <chesney.hardin@gmail.com>
2023-06-06 11:40:37 -0500

    customers can edit their profile and everyone can edit tickets

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import "./Tickets.css"
-import { useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 export const TicketList = ({ searchTermState }) => {
     const [tickets, setTickets] = useState([])
@@ -38,57 +38,57 @@ export const TicketList = ({ searchTermState }) => {
     useEffect(
         () => {
             fetch('http://localhost:8088/serviceTickets')
-            .then(response => response.json())
-            .then((ticketArray) => {
-                setTickets(ticketArray)
-            })
+                .then(response => response.json())
+                .then((ticketArray) => {
+                    setTickets(ticketArray)
+                })
         },
         [] // When this array is empty, you are observing initial component state
     )
 
-        useEffect(
-            () => {
-                if (honeyUserObject.staff) {
-                    // For employess
-                    setFiltered(tickets)
-                }
-                else {
-                    // For customers
-                    const myTickets = tickets.filter(ticket => ticket.userId === honeyUserObject.id)
-                    setFiltered(myTickets)
-                }
-            },
-            [tickets]
-        )
+    useEffect(
+        () => {
+            if (honeyUserObject.staff) {
+                // For employess
+                setFiltered(tickets)
+            }
+            else {
+                // For customers
+                const myTickets = tickets.filter(ticket => ticket.userId === honeyUserObject.id)
+                setFiltered(myTickets)
+            }
+        },
+        [tickets]
+    )
 
-        useEffect(
-            () => {
-                if (openOnly) { 
-                    const openTicketArray = tickets.filter(ticket => {
-                        return ticket.userId === honeyUserObject.id && ticket.dateCompleted === ""
-                    })
-                    setFiltered(openTicketArray)
-                }
-                else {
-                    const myTickets = tickets.filter(ticket => ticket.userId === honeyUserObject.id)
-                    setFiltered(myTickets)
-                }
-            },
-            [ openOnly ]
-        )
+    useEffect(
+        () => {
+            if (openOnly) {
+                const openTicketArray = tickets.filter(ticket => {
+                    return ticket.userId === honeyUserObject.id && ticket.dateCompleted === ""
+                })
+                setFiltered(openTicketArray)
+            }
+            else {
+                const myTickets = tickets.filter(ticket => ticket.userId === honeyUserObject.id)
+                setFiltered(myTickets)
+            }
+        },
+        [openOnly]
+    )
 
     return <>
-        { 
-            honeyUserObject.staff ? 
-            <>
-                <button onClick= { () => { setEmergency(true) } }>Emergency Only</button>
-                <button onClick= { () => { setEmergency(false) } }>Show All</button>
-            </>
-            : <>
-            <button onClick={() => navigate("/ticket/create")}>Create Ticket</button>
-            <button onClick={() => updateOpenOnly(true)}>Open Ticket</button>
-            <button onClick={() => updateOpenOnly(false)}>All My Tickets</button>
-            </>
+        {
+            honeyUserObject.staff ?
+                <>
+                    <button onClick={() => { setEmergency(true) }}>Emergency Only</button>
+                    <button onClick={() => { setEmergency(false) }}>Show All</button>
+                </>
+                : <>
+                    <button onClick={() => navigate("/ticket/create")}>Create Ticket</button>
+                    <button onClick={() => updateOpenOnly(true)}>Open Ticket</button>
+                    <button onClick={() => updateOpenOnly(false)}>All My Tickets</button>
+                </>
         }
 
 
@@ -99,7 +99,10 @@ export const TicketList = ({ searchTermState }) => {
                 filteredTickets.map(
                     (ticket) => {
                         return <section className="ticket" key={`ticket--${ticket.id}`}>
-                            <header>{ticket.description}</header>
+                            <header>
+                                <Link to={`/tickets/${ticket.id}/edit`}>Ticket {ticket.id}</Link>
+                            </header>
+                            <section>{ticket.description}</section>
                             <footer>Emergency: {ticket.emergency ? "🧨" : "No"}</footer>
                         </section>
                     }
--- src/components/views/CustomerViews.js
+++ src/components/views/CustomerViews.js
@@ -1,6 +1,8 @@
 import { Outlet, Route, Routes } from "react-router-dom"
 import { TicketForm } from "../tickets/TicketForm"
 import { TicketList } from "../tickets/TicketList"
+import { CustomerForm } from "../profile/CustomerForm"
+import { TicketEdit } from "../tickets/TicketEdit"
 
 export const CustomerViews = () => {
     return (
@@ -15,8 +17,9 @@ export const CustomerViews = () => {
             }>
 
                 <Route path="tickets" element={<TicketList />} />
-
                 <Route path="ticket/create" element={<TicketForm />} />
+                <Route path="customer/profile" element={<CustomerForm />} />
+                <Route path="tickets/:ticketId/edit" element={  <TicketEdit /> } />
 
             </Route>
         </Routes>
